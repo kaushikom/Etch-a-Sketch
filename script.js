c7263a1d0e0c15e5b4794f8de1f35e7527ccb6e7
Om <omkaushik015@gmail.com>
2022-07-26 15:48:28 +0530

    complete project

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const container = document.getElementById("container");
-let input = 5;
+let input = 10;
+let grid10 = document.getElementById("10");
+let grid15 = document.getElementById("15");
+let grid20 = document.getElementById("20");
+let reset = document.getElementById("reset");
+
+let black = document.getElementById("black");
+let grey = document.getElementById("grey");
+let rainbow = document.getElementById("rainbow");
+let random = document.getElementById("random");
+let color = "black";
+random.addEventListener("click", (e) => {
+  let r = Math.floor(Math.random() * 255);
+  let g = Math.floor(Math.random() * 255);
+  let b = Math.floor(Math.random() * 255);
+  color = `rgb(${r},${g},${b})`;
+});
+black.addEventListener("click", () => (color = "black"));
+grey.addEventListener("click", () => (color = "white"));
+
 function generateGrid(input) {
   for (let i = 0; i < input * input; i++) {
     let side = 500 / input;
@@ -11,4 +30,42 @@ function generateGrid(input) {
     console.log("pixel created");
   }
 }
-generateGrid(10);
+function removeGrid(input) {
+  for (let i = 0; i < input * input; i++) {
+    let pixel = document.querySelector("#container .pixel");
+    container.removeChild(pixel);
+  }
+}
+generateGrid(input);
+
+grid10.addEventListener("click", (e) => {
+  removeGrid(input);
+  generateGrid(e.target.id);
+  input = 10;
+});
+grid15.addEventListener("click", (e) => {
+  removeGrid(input);
+  generateGrid(e.target.id);
+  input = 15;
+});
+grid20.addEventListener("click", (e) => {
+  removeGrid(input);
+  generateGrid(e.target.id);
+  input = 20;
+});
+reset.addEventListener("click", (e) => {
+  removeGrid(input);
+  generateGrid(input);
+});
+rainbow.addEventListener("click", () => (color = "rainbow"));
+container.addEventListener("mouseover", (e) => {
+  if (color === "rainbow") {
+    console.log(1);
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    e.target.style.backgroundColor = `rgb(${r},${g},${b})`;
+  } else {
+    e.target.style.backgroundColor = `${color}`;
+  }
+});
